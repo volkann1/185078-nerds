cd08afa61620fd3e86232d7f833fb9224c9db6f0
Волкова Анна <volkann1@rambler.ru>
2016-05-06 11:34:28 +0300

    добавила свой маркер на интерактивную карту

--- js/scripts.js
+++ js/scripts.js
@@ -21,3 +21,29 @@ overlay.addEventListener("click",function(event){
   overlay.classList.remove("pop-up-show");
 });
 
+    ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [59.93970743, 30.31704635],
+            zoom: 15
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+        myPlacemark = new ymaps.Placemark([59.938631,30.323055], {
+            hintContent: 'Собственный значок метки',
+            balloonContent: 'Это красивая метка'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: 'img/map-point.png',
+            // Размеры метки.
+            iconImageSize: [231, 190],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-50, -200]
+        });
+
+    myMap.geoObjects.add(myPlacemark);
+});
+
